69f3cb92abec4f0a59551cf8875c98601ed2ec59
mieis <miei.s.0305@gmail.com>
2023-10-19 16:24:24 +0900

    app/Http/Controllers/PostController.phpの記入ミスを修正

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;git
+use Illuminate\Support\Facades\DB;
 use App\Models\Post;
 
 class PostController extends Controller
